65bc604933fb45d3691250e9b4e4533205b1b655
Nicholas <s5124083@bournemouth.ac.uk>
2020-05-08 17:33:12 +0100

    Finished most of the login interface and added other pages of the app.

---
+++ app/src/main/java/com/example/ds_fitness/homePage.kt
@@ -0,0 +1,48 @@
+package com.example.ds_fitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.LinearLayout
+import com.example.homedashboard.BMIActivity
+import com.example.homedashboard.NutritionActivity
+import com.example.homedashboard.TrackingActivity
+import com.example.homedashboard.WorkoutActivity
+
+class homePage : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity_layout)
+
+        val bmiBtn = findViewById<LinearLayout>(R.id.bmiCalculator)
+        val nutritionBtn = findViewById<LinearLayout>(R.id.nutritionPlans)
+        val workoutBtn = findViewById<LinearLayout>(R.id.workoutPlans)
+        val activityTrackingBtn = findViewById<LinearLayout>(R.id.activityTracking)
+
+        bmiBtn.setOnClickListener {
+            val intent = Intent(this, BMIActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        nutritionBtn.setOnClickListener {
+            val intent = Intent(this, NutritionActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        workoutBtn.setOnClickListener {
+            val intent = Intent(this, WorkoutActivity::class.java)
+
+            startActivity(intent)
+        }
+
+        activityTrackingBtn.setOnClickListener {
+            val intent = Intent(this, TrackingActivity::class.java)
+
+            startActivity(intent)
+        }
+    }
+
+}
--- app/src/main/java/com/example/ds_fitness/loginActivity.kt
+++ app/src/main/java/com/example/ds_fitness/loginActivity.kt
@@ -1,71 +1,60 @@
 package com.example.ds_fitness
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Typeface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class loginActivity : AppCompatActivity() {
+
+    private lateinit var enteredUsername: EditText
+    private lateinit var enteredPassword: EditText
+    private lateinit var buttonLogin : Button
+    private lateinit var buttonSignUp : Button
+
+    private lateinit var myLogicInstance: logic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
+
+        enteredUsername = findViewById(R.id.username_edit_text)
+        enteredPassword = findViewById(R.id.password_edit_text)
+        buttonLogin = findViewById(R.id.button_login)
+        buttonSignUp = findViewById(R.id.button_sign_up)
+
+        buttonLogin.setOnClickListener()
+        {
+            //Button click will call a separate validation method
+            validateLoginAttempt(enteredUsername.text.toString(), enteredPassword.text.toString())
+        }
+
     }
 
-    //Grey 160,160,160
-    //Cyan 51.153.255
-
-    //Set the color values
-
-
-    //Set login screen widgets?
-    //val myEditText = EditText
-
-    public fun onLoginClick(view: View)
+    private fun validateLoginAttempt(recUserName: String, recPassword: String)
     {
-        //On click function simply calls valdation methods, to decouple the logic from the implementation
-
+        if(myLogicInstance.validate(recUserName, recPassword))
+        {
+            //progress to the homepage
+            val intent = Intent(this, homePage::class.java)
+            intent.putExtra("ID_EXTRA", recUserName)
+            startActivity(intent)
+        }
+        else
+        {
+            //Get interface to display message to user
+            showFailedLoginState()
+        }
 
     }
 
-    private fun validateLoginAttempt(recUserName: String, recPassword: String): Boolean
+    private  fun showFailedLoginState()
     {
-        var retVal: Boolean = false
+        //Change the login page layout so that it tells the user that the previous login attempt
+        // failed.
 
-        return retVal
+
     }
 
-    //override fun onDraw(canvas: Canvas)
-    //{
-        //super.onDraw(canvas)
-
-        //val screenWidth = width.toFloat()
-        //val screenHeight = height.toFloat()
-        //val bannerHeight = screenHeight / 7
-
-        //Rectangle parameters
-        //left - The X coordinate of the left side of the rectangle
-        //top - The Y coordinate of the top side of the rectangle
-        //right - The X coordinate of the right side of the rectangle
-        //bottom - The Y coordinate of the bottom side of the rectangle
-        //paint - The paint object that will be used to display the rectangle
-
-        //Draw background
-        //canvas.drawRect(0f, 0f, screenWidth, screenHeight, myBackPaint)
-
-        //Draw top and bottom rectangles
-        //canvas.drawRect(0f, 0f, screenWidth, bannerHeight, myTopAndBottomBandsPaint)
-        //canvas.drawRect(0f, screenHeight - bannerHeight, screenWidth, screenHeight, myTopAndBottomBandsPaint)
-
-        //Draw login page title
-        //canvas.drawText("DS Fitness", (screenWidth / 2), (bannerHeight / 2) + 20, myTextPaint)
-
-        //Draw login rectangle
-        //canvas.drawRect((screenWidth / 5), screenWidth * 2, screenWidth * 4, bannerHeight * 5, myLoginBoxPaint)
-
-
-    //}
 }
