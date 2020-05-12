ef37b8dd7d3c4ab6aba98ca5fab0f0fa8393a53f
Nicholas <s5124083@bournemouth.ac.uk>
2020-05-12 16:29:40 +0100

    Fixed an issue where the app's homepage would fail to be created.

--- app/src/main/java/com/example/ds_fitness/LoginActivity.kt
+++ app/src/main/java/com/example/ds_fitness/LoginActivity.kt
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.view.View
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var textLoginFailed: TextView
     private lateinit var enteredUsername: EditText
     private lateinit var enteredPassword: EditText
     private lateinit var buttonLogin : Button
     private lateinit var buttonSignUp : Button
-
-    private lateinit var myLogicInstance: logic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
         enteredPassword = findViewById(R.id.password_edit_text)
         buttonLogin = findViewById(R.id.button_login)
         buttonSignUp = findViewById(R.id.button_sign_up)
-
+        textLoginFailed = findViewById(R.id.login_failed_text)
 
         buttonLogin.setOnClickListener()
         {
@@ -35,7 +36,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun validateLoginAttempt(recUserName: String, recPassword: String)
     {
-        if(myLogicInstance.validate(recUserName, recPassword))
+        if(Logic.validate(recUserName, recPassword))
         {
             //progress to the homepage
             val intent = Intent(this, homePage::class.java)
@@ -49,12 +50,23 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private  fun showFailedLoginState()
+    private fun showFailedLoginState()
     {
         //Change the login page layout so that it tells the user that the previous login attempt
         // failed.
+        showView(textLoginFailed as View)
 
+    }
 
+    //May be better if this was moved to the logic class
+    private fun showView(view: View)
+    {
+        view.visibility = View.VISIBLE
+    }
+
+    private fun hideView(view: View)
+    {
+        view.visibility = View.GONE
     }
 
 }
--- app/src/main/java/com/example/ds_fitness/homePage.kt
+++ app/src/main/java/com/example/ds_fitness/homePage.kt
@@ -3,13 +3,23 @@ package com.example.ds_fitness
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.LinearLayout
+import java.util.logging.Logger
 
 class homePage : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.homepage_layout)
+
+        try {
+            setContentView(R.layout.homepage_layout)
+        }
+        catch (e: Exception)
+        {
+            Logger.getLogger(homePage::class.java.name).warning("An error occured while trying to create the activity: " + e.toString())
+            throw e
+        }
 
         val bmiBtn = findViewById<LinearLayout>(R.id.bmiCalculator)
         val nutritionBtn = findViewById<LinearLayout>(R.id.nutritionPlans)
